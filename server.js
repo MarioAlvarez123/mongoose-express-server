affd34c5e4b34fc497d88fb0c00ca0b4392bdd7b
Mario Alvarez <Malvar322@west-mec.org>
2024-11-12 13:48:47 -0700

    adding files

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("require");
+const dotenv = require("dotenv");
+
+if (fs.existsSync('.env')) {
+    require('dotenv').config();
+}
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+const connections = {}
+
+const models = {}
+
+const bankUserSchema = new mongoose.Schema({});
+
+const getConnection = async (dbName) => { 
+    console.log('getConnection called with dbName');
+    
+    if (connections 
+}
+
